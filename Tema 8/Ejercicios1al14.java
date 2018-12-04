2b3665ae6ee5ea307adb693f6d73cdf86f6c3fd7
Adrian Jimenez Montilla <er_adri_14_7@hotmail.com>
2018-12-04 13:05:39 +0100

    Agregado ejercicios Tema 8

--- Tema 8/Ejercicios1al14.java
+++ Tema 8/Ejercicios1al14.java
@@ -11,7 +11,7 @@ import matematicas.varias;
 public class Ejercicios1al14 {
 
   public static void main(String[] args) {
-  
+    // Es Capicua //
     if (matematicas.varias.esCapicua(49)) {
       System.out.println("El 49 es capicúa");
     }
@@ -19,5 +19,24 @@ public class Ejercicios1al14 {
     if (matematicas.varias.esCapicua(767)) {
       System.out.println("El 767 es capicúa");
     }
+    // Es primo //
+    if (matematicas.varias.esPrimo(7)) {
+      System.out.println("El 7 es primo ");
+    }
+    if (matematicas.varias.esPrimo(25)) {
+      System.out.println("El 25 es primo ");
+    }
+    // Siguiente primo
+    System.out.println ("El siguiente primo a 7 es: " + matematicas.varias.siguientePrimo(7));
+    System.out.println ("El siguiente primo a 25 es: " + matematicas.varias.siguientePrimo(25));
+    
+    // Voltea el numero //
+    System.out.println ("El 9 volteado es " + matematicas.varias.voltea(9));
+    System.out.println ("El 56789 volteado es " + matematicas.varias.voltea(56789));
+    
+    // Potencia de un numero //
+    System.out.println ("2 elevado a 8 es: " + matematicas.varias.potencia(2, 8));
+    System.out.println ("9 elevado a 5 es: " + matematicas.varias.potencia(9, 5));
+    
   }
 }
--- Tema 8/matematicas/varias.java
+++ Tema 8/matematicas/varias.java
@@ -38,11 +38,78 @@ public static boolean esCapicua(int x) {
   return esCapicua((long)x);
 }
 
+/**
+* Ejercicio 2
+* Devuelve verdadero si el número que se pasa como parámetro es primo y falso
+* en caso contrario.
+* <p>
+* Un número primo es un número natural mayor que 1 y divisible únicamente
+* entre el mismo y entre 1.
+*
+* @param x número del que se quiere saber si es primo
+* @return verdadero si el número que se pasa como parámetro es primo y falso
+*
+en caso contrario
+*/
 
+public static boolean esPrimo(long n) {
+  if (n < 2) {
+    return false;
+  } else {
+    for (long i = n / 2; i >= 2; i--) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+/**
+* Ejercicio3
+* Devuelve el menor primo que es mayor al número que se pasa como parámetro.
+*
+* @param x un número entero
+* @return el menor primo que es mayor al número que se pasa como parámetro
+*/
+public static int siguientePrimo (int x) {
+  while (!esPrimo(++x)) {};
+  return x;
+}
 
-
-
-
+/**
+* Ejercicio4
+* Dada una base y un exponente, devuelve la potencia.
+*
+* @param base
+* base de la potencia
+* @param exponente exponente de la potencia
+* @return
+número resultante de elevar la base a la potencia indicada
+*/
+public static double potencia (int base, int exponente) {
+  if (exponente == 0) {
+    return 1;
+  }
+  if (exponente < 0) {
+    return 1/potencia(base, -exponente);
+  }
+  
+  int n = 1;
+  
+  for ( int i = 0; i < Math.abs(exponente); i++) {
+    n = n * base;
+  }
+  
+  return n;
+}
+/**
+* Ejercicio 5
+* Cuenta el número de dígitos de un número entero.
+*
+* @param x número al que se le quieren contar los dígitos
+* @return número de dígitos que tiene el número que se pasa como parámetro
+*/
+    
 
 
 
