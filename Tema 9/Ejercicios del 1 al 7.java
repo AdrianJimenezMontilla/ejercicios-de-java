b8d35a3bd657d44555e42abfe918fe6e9c286c7b
Adrian Jimenez Montilla <er_adri_14_7@hotmail.com>
2019-01-01 04:02:06 +0100

    Agregado ejercicios Tema 9

---
+++ Tema 9/Ejercicios del 1 al 7.java
@@ -0,0 +1,48 @@
+/**
+*  
+* 
+* 
+* 
+* 
+* @author Adrian Jimenez Montilla
+*
+*
+*1.¿Cuáles serían los atributos de la clase PilotoDeFormula1 ? ¿Se te ocurren algunas
+*instancias de esta clase?
+* Edad, Nombre, Titulos, Nacionalidad, Numero de carreras
+* Instancias: Fernando Alonso,De la Rosa, Sainz, etc
+*2. A continuación tienes una lista en la que están mezcladas varias clases con
+* instancias de esas clases. Para ponerlo un poco más difícil, todos los elementos
+* están escritos en minúscula. Di cuáles son las clases, cuáles las instancias,
+* a qué clase pertenece cada una de estas instancias y cuál es la jerarquía
+* entre las clases: paula, goofy, gardfiel, perro, mineral, caballo, tom, silvestre,
+* pirita, rocinante, milu, snoopy, gato, pluto, animal, javier, bucefalo, pegaso,
+* ayudante_de_santa_claus, cuarzo, laika, persona, pato_lucas.
+* Clases: perro, mineral, caballo, gato,animal, persona, cuarzo, pirita
+* jerarquia: Animal | |---Perro | |---goofy | |---milu | |---snoopy | |---pluto | |---ayudante_de_santa_claus | |---laika | |---Caballo | |---rocinante | |---bucefalo | |---pegaso | |---Gato | |---garfield | |---tom | |---silvestre | |---Persona | |---paula | |---javier | |---pato_lucas
+* mineral pirita y cuarzo
+
+*3.¿Cuáles serían los atributos de la clase Vivienda ? ¿Qué subclases se te ocurren?
+*Numero de habitaciones, metros cuadrado, ubicacion
+*Sublases: pisos, adosados, cortijo, chalet
+
+*4. Piensa en la liga de baloncesto, ¿qué 5 clases se te ocurren para representar
+*5 elementos distintos que intervengan en la liga?
+*Jugador, Partido, Estadio, Equipo, Arbitro
+
+*5. Haz una lista con los atributos que podría tener la clase caballo. A continuación
+*haz una lista con los posibles métodos (acciones asociadas a los caballos).
+*atributos: nombre, raza, color, edad, carrerasGanadas
+* cabalga, relincha, trota, rumia
+
+*6. Lista los atributos de la clase Alumno ¿Sería nombre uno de los atributos de la
+*clase? Razona tu respuesta.
+
+* numExpediente, nombre, fechaNacimiento, curso, direccion, telefono
+* es un atributo que guarda el nombre completo con tildes, espacios, mayúsculas y minúsculas, etc
+
+*7. ¿Cuáles serían los atributos de la clase Ventana (de ordenador)? ¿cuáles serían los métodos? Piensa en las propiedades y en el comportamiento de una ventana de cualquier programa.
+
+* Atributos de la clase Ventana: esVisible, tieneElFoco, posicion, anchura, altura, colorDeFondo, titulo, esRedimensionable
+
+*Métodos de la clase Ventana: maximiza, minimiza, restaura, cierra, redimiensiona, mueve
